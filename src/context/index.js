8eb3ec1611aef249238bf7ca25430c6e3e8341c0
Rajat sood <rsood2185@gmail.com>
2024-10-02 15:42:34 +0530

    udpated

--- src/context/index.js
+++ src/context/index.js
@@ -89,7 +89,7 @@ function reducer(state, action) {
       return {
         ...state,
         loading: false,
-        notifications: new Notification().fromArray(action.value),
+        notifications: new Notification().fromArray(action.value?.reverse()),
       };
     }
     case "DIALOG": {
